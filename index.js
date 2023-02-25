da947f24ea40c0fb5622d3bdfeb89ac9a75e2183
Leônidas Augusto Amaro <augustoamaro@icloud.com>
2023-02-25 17:53:43 -0300

    Adjusting css

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const questions = document.querySelector('.questions')
 const awnsers = document.querySelector('.anwsers')
 
 questions.addEventListener('click', () => {
+    // questions.children[0].style.color = '#1E1F36';
+    // questions.children[0].style.fontWeight = '700';
+
+    console.dir(questions)
     if (awnsers.classList.contains('anwsers')) {
         awnsers.classList.remove('anwsers');
     } else {
         awnsers.classList.add('anwsers');
     }
-})
+});
