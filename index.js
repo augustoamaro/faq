385023b861541235c0bbd4b7850ed4652cde0503
Leônidas Augusto Amaro <augustoamaro@icloud.com>
2023-02-25 23:55:07 -0300

    Fixed questions div

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const questions = document.querySelector('.questions')
-const awnsers = document.querySelector('.anwsers')
+const questions = document.querySelectorAll('.question')
+const answers = document.querySelectorAll('.answer')
 
-questions.addEventListener('click', () => {
-    // questions.children[0].style.color = '#1E1F36';
-    // questions.children[0].style.fontWeight = '700';
+for (let i = 0; i < questions.length; i++) {
+    const question = questions[i];
 
-    console.dir(questions)
-    if (awnsers.classList.contains('anwsers')) {
-        awnsers.classList.remove('anwsers');
-    } else {
-        awnsers.classList.add('anwsers');
-    }
-});
+    question.addEventListener('click', (e) => {
+        console.log(e)
+
+        if (e.target.style.display === 'none') {
+            question.children[0].style.color = '#1E1F36';
+            question.children[0].style.fontWeight = '700';
+            question.children[1].style.transform = 'rotate(180deg)';
+            e.target.style.display = 'flex';
+        } else {
+            question.children[0].style.color = '#4B4C5F';
+            question.children[0].style.fontWeight = '400';
+            question.children[1].style.transform = 'rotate(0deg)';
+            e.target.style.display = 'none';
+        }
+
+
+
+
+    });
+
+
+
+}
+
+
+
